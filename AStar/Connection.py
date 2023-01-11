c8d3f5f2f33ab00d986adbfa0b83968f726b35cf
Peter Burgess <pburgess2003@gmail.com>
2023-01-11 21:39:57 +1030

    Static method to create connections added to Connection.

--- AStar/Connection.py
+++ AStar/Connection.py
@@ -7,3 +7,25 @@ class Connection:
         self.node2: Node = node2
 
         self.weight: int = weight
+
+    @staticmethod
+    def create_connection_between(node1: Node, node2: Node, weight: int) -> None:
+        """
+Connects the two nodes by creating a connection class and putting it inside the connections list of both nodes.
+Does nothing if there is already a connection.
+        """
+
+        # Checking for a connection
+        for connection in node1.connections:
+            if connection.node1 == node1 and connection.node2 == node2:
+                return
+
+            if connection.node1 == node2 and connection.node2 == node1:
+                return
+
+        # If no connection than create one
+        new_connection = Connection(node1, node2, weight)
+
+        # Add it to the nodes
+        node1.connections.append(new_connection)
+        node2.connections.append(new_connection)
--- AStar/Grid.py
+++ AStar/Grid.py
@@ -14,16 +14,12 @@ class Grid:
         # Add horizontal connections
         for y in range(y_size):
             for x in range(x_size - 1):
-                new_connection = Connection(self.grid[y][x], self.grid[y][x + 1], 1)
-                self.grid[y][x].connections.append(new_connection)
-                self.grid[y][x + 1].connections.append(new_connection)
+                Connection.create_connection_between(self.grid[y][x], self.grid[y][x + 1], 1)
 
         # Add vertical connections
         for y in range(y_size - 1):
             for x in range(x_size):
-                new_connection = Connection(self.grid[y][x], self.grid[y + 1][x], 1)
-                self.grid[y][x].connections.append(new_connection)
-                self.grid[y + 1][x].connections.append(new_connection)
+                Connection.create_connection_between(self.grid[y][x], self.grid[y + 1][x], 1)
 
     def __getitem__(self, coords: tuple[int, int]) -> Node:
         """
