b1f90e1e3424106d5345f3e4a61503d91e055ddd
Peter Burgess <pburgess2003@gmail.com>
2023-01-11 23:09:08 +1030

    Better package init file

--- AStar/__init__.py
+++ AStar/__init__.py
@@ -1,5 +1,12 @@
+# Errors, huh
 import AStar.Errors
+
+# The basics, need to come first as they are used by almost everything
 from ._Node import Node
 from ._Connection import Connection
+
+# Ways of generating sets of nodes and connections
 from ._Grid import Grid
+
+# The algorithm
 from ._AStar import AStar
