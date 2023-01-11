f7ce8f21c41e45138c04a3d68a4f99e0681044da
Peter Burgess <pburgess2003@gmail.com>
2023-01-11 22:47:51 +1030

    Position, Reset and DocStrings added.

--- AStar/Node.py
+++ AStar/Node.py
@@ -6,11 +6,15 @@ if TYPE_CHECKING:
 
 
 class Node:
-    def __init__(self):
+    def __init__(self, x_position: int, y_position: int):
+        # Position
+        self.x_position = x_position
+        self.y_position = y_position
+
         # Distance from the starting node
-        self.g_cost: int = 0
+        self.g_cost: int | None = None
         # Distance from the end node (heuristic)
-        self.h_host: int = 0
+        self.h_host: int | None = None
 
         # The connections to other nodes
         self.connections: list['Connection'] = []
@@ -22,6 +26,16 @@ class Node:
     def f_cost(self):
         # G cost + H cost
         return self.g_cost + self.h_host
+
+    def reset(self):
+        """
+Resets the node's G and H costs, connections and parent.
+        """
+
+        self.g_cost = None
+        self.h_host = None
+        self.connections = []
+        self.parent = None
 
     def get_connected_nodes(self) -> list['Node']:
         """
@@ -41,6 +55,12 @@ Returns a list of the connected nodes found in self.connections.
 
     @staticmethod
     def get_all_lowest_f_cost(list_of_nodes: list['Node']) -> list['Node']:
+        """
+Returns a list of all nodes that have the lowest F cost in the given list.
+        :param list_of_nodes: List of nodes to be handled.
+        :return: List of nodes with the lowest F cost.
+        """
+
         # Get the minimum F cost
         min_f_cost = min([node.f_cost for node in list_of_nodes])
 
@@ -51,6 +71,25 @@ Returns a list of the connected nodes found in self.connections.
 
         # Return them
         return  all_min_f_cost_nodes
+
+    @staticmethod
+    def get_all_lowest_h_cost(list_of_nodes: list['Node']) -> list['Node']:
+        """
+Returns a list of all nodes that have the lowest H cost in the given list.
+        :param list_of_nodes: List of nodes to be handled.
+        :return: List of nodes with the lowest H cost.
+        """
+
+        # Get the minimum F cost
+        min_h_cost = min([node.h_cost for node in list_of_nodes])
+
+        # Get all node with the same minimum F cost
+        all_min_h_cost_nodes = [
+            node for node in list_of_nodes if node.h_cost == min_h_cost
+        ]
+
+        # Return them
+        return  all_min_h_cost_nodes
 
     def __str__(self):
         return f"Node - G: {self.g_cost}, H: {self.h_host}, F: {self.f_cost}"
