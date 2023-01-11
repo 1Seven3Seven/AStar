3c46befbdcbcd1683329cee0c2f484e8e3bc3a8a
Peter Burgess <pburgess2003@gmail.com>
2023-01-11 21:40:14 +1030

    Function to return all connected nodes for Node

--- AStar/Node.py
+++ AStar/Node.py
@@ -16,12 +16,28 @@ class Node:
         self.connections: list['Connection'] = []
 
         # The node which points to this one
-        self.parent: Node | None = None
+        self.parent: 'Node' | None = None
 
     @property
     def f_cost(self):
         # G cost + H cost
         return self.g_cost + self.h_host
+
+    def get_connected_nodes(self) -> list['Node']:
+        """
+Returns a list of the connected nodes found in self.connections.
+        """
+
+        connected_nodes: list['Node'] = []
+
+        for connection in self.connections:
+            if connection.node1 != self:
+                connected_nodes.append(connection.node1)
+                continue
+
+            connected_nodes.append(connection.node2)
+
+        return connected_nodes
 
     def __str__(self):
         return f"Node - G: {self.g_cost}, H: {self.h_host}, F: {self.f_cost}"
