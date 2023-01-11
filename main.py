293feef1ba7b22d5fb551f9718fbef0a3edd9b5a
Peter Burgess <pburgess2003@gmail.com>
2023-01-12 00:19:52 +1030

    Main file showing off stuff.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import AStar
+from AStar import AsciiPrinter
+
+
 def main():
-    ...
+    grid = AStar.Grid(10, 10)
+
+    a_star = AStar.AStar(start=grid[0, 0], end=grid[9, 9])
+
+    path = a_star.find_path()
+
+    print("\nNode Parents\n")
+    AsciiPrinter.print_grid_node_parents(grid)
+
+    print("\nPath Generated\n")
+    AsciiPrinter.print_grid_with_path(grid, path)
+
+    print()  # Spacing
+    print(f"Passes performed {a_star.passes_performed}")
+    print()  # Spacing
 
 
 if __name__ == "__main__":
     main()
+
+    input("Enter to exit")
