ccd38a3ea35685cb5711d39119028da6bef84c43
Peter Burgess <pburgess2003@gmail.com>
2023-01-12 16:06:31 +1030

    Better name

--- AStar/AsciiPrinter/__init__.py
+++ AStar/AsciiPrinter/__init__.py
@@ -1,3 +1,3 @@
 from ._path_lookup import path_lookup
-from .print_grid import print_grid_node_parents
+from .print_grid_node_parents import print_grid_node_parents
 from .print_grid_with_path import print_grid_with_path
